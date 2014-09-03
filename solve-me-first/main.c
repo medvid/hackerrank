bde05c52ac71e63ccc1ab4c0294b7a70b9008152
Volodymyr Medvid <vmedvid@riseup.net>
2014-09-03 23:42:48 +0300

    solve-me-first: add solution

---
+++ solve-me-first/main.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    int a, b;
+    scanf("%i %i", &a, &b);
+    printf("%i\n", a + b);
+    return 0;
+}
